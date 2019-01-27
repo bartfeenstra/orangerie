2cb39efaba2f4b7bd45acf5facc36fc38babf299
Bart Feenstra <bart@mynameisbart.com>
2019-01-27 13:58:03 +0000

    Do not depend on non-existent directories.

--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,6 @@ module.exports = {
       path: path.resolve(__dirname, 'build', 'index.html'),
       template: 'index.html'
     }),
-    new webpack.HashedModuleIdsPlugin(),
-    new CopyWebpackPlugin([
-      { from: path.resolve(__dirname, 'assets'), to: path.resolve(__dirname, 'build', 'assets') }
-    ])
+    new webpack.HashedModuleIdsPlugin()
   ]
 }
